b2d0948cc38d0838645f904e4a22f9a193e8562b
telion85 <www.neon@mail.ru>
2023-05-26 16:24:06 +0500

    adding java docs for services

--- src/main/java/ga/heaven/marketplace/service/AdService.java
+++ src/main/java/ga/heaven/marketplace/service/AdService.java
@@ -11,19 +11,63 @@ import java.util.List;
 
 public interface AdService {
     ResponseWrapperAdsDto getAds();
+
+    /**
+     * Create new ad and add it to the repository
+     * @param properties data for ad
+     * @param image picture with ad image
+     * @param userName ad creator username
+     */
     AdDto addAds(CreateAdDto properties, ImageModel image, String userName);
-    
+
+    /**
+     * Get full information about ad
+     * @param id ad's id
+     * @return full info about ad
+     */
     FullAdDto getFullAd(long id);
 
+    /**
+     * Remove ad by id
+     * @param id ad's id
+     * @return http code
+     */
     int removeAd(long id);
 
+    /**
+     * Update ad by id
+     * @param id ad's id
+     * @param createAdDto new data for update ad
+     * @return updated ad
+     */
     AdDto updateAd(long id, CreateAdDto createAdDto);
-    
+
+    /**
+     * Get list of ads by user
+     * @param username user name
+     * @return list of user ads
+     */
     ResponseWrapperAdsDto getAdsMe(String username);
 
+    /**
+     * Update image for ad
+     * @param ad editing ad
+     * @param image new image for ad
+     * @return edited ad
+     */
     AdModel updateAdImage(AdModel ad, ImageModel image);
-    
+
+    /**
+     * Find ad, which containing string in title
+     * @param searchTitle search string
+     * @return list of eligible ad
+     */
     List<AdDto> findByTitleContainingIgnoreCase(String searchTitle);
 
+    /**
+     * Get ad by id
+     * @param id ad's id
+     * @return eligible ad
+     */
     AdModel getAdById(long id);
 }
--- src/main/java/ga/heaven/marketplace/service/AuthService.java
+++ src/main/java/ga/heaven/marketplace/service/AuthService.java
@@ -4,6 +4,20 @@ import ga.heaven.marketplace.dto.RegisterReqDto;
 import ga.heaven.marketplace.dto.Role;
 
 public interface AuthService {
+
+    /**
+     * Authorization user
+     * @param userName login for enter
+     * @param password password for enter
+     * @return authorization success
+     */
     boolean login(String userName, String password);
+
+    /**
+     * adding a new user to the database
+     * @param registerReqDto user data for new user
+     * @param role role for new user
+     * @return registration success
+     */
     boolean register(RegisterReqDto registerReqDto, Role role);
 }
