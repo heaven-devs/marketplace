6210934c24fce62519ee595b81daafb96eba596b
telion85 <www.neon@mail.ru>
2023-05-26 16:24:06 +0500

    adding java docs for services

--- src/main/java/ga/heaven/marketplace/service/CommentService.java
+++ src/main/java/ga/heaven/marketplace/service/CommentService.java
@@ -5,8 +5,19 @@ import ga.heaven.marketplace.dto.RequestWrapperCommentDto;
 import ga.heaven.marketplace.dto.ResponseWrapperCommentsDto;
 
 public interface CommentService {
+
+    /**
+     * Get comment for current ad
+     * @param rq current ad
+     * @return list of comments
+     */
     ResponseWrapperCommentsDto getComments(RequestWrapperCommentDto rq);
-    
+
+    /**
+     * Add comment
+     * @param rq current ad and new comment
+     * @return edited comment
+     */
     CommentDto addComment(RequestWrapperCommentDto rq);
     
     boolean isMine(RequestWrapperCommentDto rq);
--- src/main/java/ga/heaven/marketplace/service/UserService.java
+++ src/main/java/ga/heaven/marketplace/service/UserService.java
@@ -11,15 +11,49 @@ public interface UserService {
 
     UserDto getCurrentUser(String username);
 
+    /**
+     * Update current authorized user
+     * @param authUser current authorized user
+     * @param userDto new user data
+     * @return updated user
+     */
     UserModel updateUser(UserModel authUser, UserDto userDto);
 
+    /**
+     * Get user entity by username
+     * @param username user name
+     * @return user entity
+     */
     UserModel getUser(String username);
 
+    /**
+     * Set password for current authorized user
+     * @param authUser current authorized user
+     * @param newPasswordDto new password
+     * @return updated user
+     */
     UserDto setUserPassword(UserModel authUser, NewPasswordDto newPasswordDto);
 
+    /**
+     * Load new user image (avatar)
+     * @param authUser current authorized user
+     * @param image new image
+     * @return updated user
+     */
     UserDto loadUserImage(UserModel authUser, ImageModel image);
 
+    /**
+     * Password validation check
+     * @param authUser current authorized user
+     * @param currentPassword current user password
+     * @return true if the password is correct
+     */
     boolean isPasswordCorrect(UserModel authUser, String currentPassword);
 
+    /**
+     * Create new user
+     * @param registerReqDto new user details
+     * @param role role for new user
+     */
     void createUser(RegisterReqDto registerReqDto, Role role);
 }
